588977e2a22e4d801a093face587a39f9553fbc3
Cooper <laobocooper@gmail.com>
2022-01-06 17:10:27 +0800

    Done.

--- plot.R
+++ plot.R
@@ -13,46 +13,62 @@ if (!require("ggplot2")) install.packages("pacman")
 if (!require("seqinr")) install.packages("seqinr")
 if (!require("dplyr")) install.packages("dplyr")
 if (!require("showtext")) install.packages("showtext")
+if (!require("plotly")) install.packages("plotly")
+
+
 
 #load the library
 require(ggplot2)
 require(seqinr)
 require(dplyr)
 require(showtext) # this library can deal with chinese!
+require(plotly)
+
+###plot all files that contain suffixes seem like c(".fa",".fas", ".fasta" ) in current directory
+
+seq.files<-list.files()
+
+for (seq.files in seq.files[grep(".fa$|.fas$|.fasta$", seq.files) ] ){
+
+  ####### process fasta files#####
+  seq.fa<-read.fasta(seq.files,seqtype = "DNA")
+
+  # remove duplicated seq
+  seq.fa<-seq.fa[!duplicated(names(seq.fa))]
+
+  #cut the name of each sequence, leaving  only the first three prefixes
+
+  names(seq.fa)<- unlist(lapply(strsplit(names(seq.fa),split = "_"), function(x){
+    paste0(x[1],"_",x[2],"_",x[3])
+  }),use.names = F)
 
 
-####### process fasta files#####
-seq.fa<-read.fasta("~/Documents/DNAbarcode/附件2鱼类条形码序列文件-final-NOgap.fas",seqtype = "DNA")
+  ## plot the DNA barcode
+  cc<-lapply(seq.fa,function(x){
+    bc=data.frame(group=as.character(x))
+    bc <-bc %>%  dplyr::filter(group %in% c("a","g","c","t")) # remove any base not in c("a","g","c","t")
+    bc$xpos<-rep_len(0:nrow(bc)*0.15,length.out = nrow(bc))
+    return(bc)
+  })
+
+  cc<-bind_rows(cc,.id = "type")
+
+  showtext_auto()
+  p<-ggplot()+geom_rect(data=cc,aes(xmin=xpos,xmax=xpos+0.15,ymin=0,ymax=1,show.legend = F,fill=group))+
+    facet_wrap(vars(type),ncol = 1,strip.position = "top")+
+    theme_void()+scale_fill_manual(values = c("#d11141","#00b159","#ffc425","#00aedb"))+
+    theme(strip.background = element_blank(),
+          strip.text.x = element_text(hjust = 0.068,size = 6),
+          legend.position = "none",
+          panel.spacing = unit(1,"lines")) #increase facet_warp space
+
+  pdf(paste0(seq.files,".pdf"),width = 8,height = length(table(cc$type))/2)
+  plot(p)
+  dev.off()
 
 
-# remove duplicated seq
-seq.fa<-seq.fa[!duplicated(names(seq.fa))]
+}
 
-## plot the DNA barcode
-cc<-lapply(seq.fa,function(x){
-  bc=data.frame(group=as.character(x))
-  bc$xpos<-rep_len(0:nrow(bc)*0.15,length.out = nrow(bc))
-  return(bc)
-})
-
-cc<-bind_rows(cc,.id = "type")
-
-
-# remove any "n" base
-cc %>% dplyr::filter(group %in% c("a","g","c","t"))->cc
-
-showtext_auto()
-p<-ggplot()+geom_rect(data=cc,aes(xmin=xpos,xmax=xpos+0.15,ymin=0,ymax=1,show.legend = F,fill=group))+
-  facet_wrap(vars(type),ncol = 1,strip.position = "top")+
-  theme_void()+scale_fill_manual(values = c("#d11141","#00b159","#ffc425","#00aedb"))+
-  theme(strip.background = element_blank(),
-        strip.text.x = element_text(hjust = 0.068,size = 6),
-        legend.position = "none",
-        panel.spacing = unit(1,"lines")) #increase facet_warp space
-
-pdf("~/Documents/DNAbarcode/p.pdf",width = 8,height = length(table(cc$type))/2)
-plot(p)
-dev.off()
 
 
 ####
